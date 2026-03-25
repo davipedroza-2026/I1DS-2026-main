cb09394258a6121a3f7b9af4813e7c38c3531984
davipedroza-2026 <davi.pedroza@edu.senai.br>
2026-03-25 11:20:39 -0300

    ativoidades do dia

--- ativividades/parte-01-variaveis-Operacoeslogicas.js
+++ ativividades/parte-01-variaveis-Operacoeslogicas.js
@@ -1,10 +1,30 @@
 // criando variáveis com nome 
 var nome = "matheus";
-console.log(nome);
-
-console.log("meu nome é", nome);
 
 // criando variável com a idade 
 var idade = 35; 
 
-console.log("Meu nome é", nome, "eu tenho", idade, "anos \n");
+// criando varíavel de cidade
+var cidade = "são paulo"
+
+console.log("Meu nome é", nome, "eu moro em ", cidade, "e eu tenho", idade, "anos \n");
+ 
+// atividade 2 //
+console.log("------------------------------------------------------------------------") 
+ // variavel de conta 
+ let x = 10;
+ let y = 7; 
+ console.log("x =", x, "\ny =", y);
+ console.log("------------------"); 
+
+ let soma = x + y;
+ console.log("A soma de x + y =", soma); 
+
+let subtracao = x - y;
+ console.log("A subtração de x - y =", subtracao);
+
+ let multiplicacao = x * y;
+ console.log("A multiplicação de x * y", multiplicacao); 
+
+ let divisao = x / y;
+ console.log("A deivisão de x / y", divisao);
